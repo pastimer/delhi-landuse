1e4980bb554891b3105464b1fcd1af822f431f91
pastimer <pastimer@github>
2015-09-24 20:56:18 +0530

    CLU of july, 2015
    added the notifications and updated data of SO 2047 of 27-07-2015.

--- data/dataCLU.js
+++ data/dataCLU.js
@@ -327,6 +327,9 @@ var dataCLU = [
 {"SO": 1412, "date": "2015-05-25", file: "K-13011/27/2014-DD-I", changes: [
   {"Sl": "0", "location": "Proposed National Investigation Agency (NIA) Headquarter Building, opposite CGO Complex, opening on Road to JLN  Stadium" , "from": {"useZone": "R", "subCode": "D", "subUse": ""}, "to": {"useZone": "G", "subCode": "2", "subUse": "Govt. Office"}, "area": 0.4191, "zone": "D"}
 ]},
+{"SO": 2047, "date": "2015-07-27", file: "K-13011/29/2014-DD-I", changes: [
+{"Sl": "0", "location": "PVC Bazar Project, Tikri Kalan, Rohtak Road" , "from": {"useZone": "M", "subCode": "1", "subUse": "Manufacturing, Service & Repair Industries"}, "to": {"useZone": "U", "subCode": "4", "subUse": "Solid Waste Management Facility"}, "area": 1.7540, "zone": "L"}
+]},
 {"SO": 2048, "date": "2015-07-27", file: "K-13011/22/2012-DD-I", changes: [
 {"Sl": "0", "location": "Plot adjacent to Hotel Leela in Moti Bagh" , "from": {"useZone": "T", "subCode": "3", "subUse": "Rail Circulation"}, "to": {"useZone": "R", "subCode": "D", "subUse": ""}, "area": 0.783, "zone": "D"}
 ]}
